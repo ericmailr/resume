ce3659b15b05dc0d834516ec045103362bda2f45
Eric Miller <ericmailr@gmail.com>
2019-07-09 15:43:14 -0600

    add script to adapt to window sizes

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const wrapper = document.getElementById('wrapper');
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+width = width - 20;
+wrapper.style.width = width + 'px';
