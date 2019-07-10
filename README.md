5a579b32bcc3f448bea3ba6b2d9acb455239c48c
Eric Miller <ericmailr@gmail.com>
2019-07-09 21:39:20 -0600

    add nav active status javascript

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Eric Miller
 
 Resume Website. View here: https://ericmailr.github.io/resume/
 
-Todo: Add javascript to copy email address / phone number. Add javascript to navagation links. Add javascript to improve link underlines.
+Todo: Add javascript to copy email address / phone number. Add javascript to make underlined links look fancier.
--- script.js
+++ script.js
@@ -1,5 +1,65 @@
-
+/* make contents adaptable to window width */
 const wrapper = document.getElementById('wrapper');
 var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 width = width - 20;
 wrapper.style.width = width + 'px';
+
+
+/* add .active class to section when scrolled to */
+var projectsContainer = document.getElementById('projects-container');
+var skillsContainer = document.getElementById('skills-container');
+var experienceContainer = document.getElementById('experience-container');
+var aboutContainer = document.getElementById('about-container');
+
+var navLinks = document.getElementsByClassName('nav-link');
+var projectsLink = navLinks[0].parentElement;
+var skillsLink = navLinks[1].parentElement;
+var experienceLink = navLinks[2].parentElement;
+var aboutLink = navLinks[3].parentElement;
+
+const updateNavLinks = () => {
+    var windowTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var windowHeight = window.innerHeight;
+    var docHeight = Math.max( document.body.scrollHeigh || document.documentElement.scrollHeight );
+    if (windowTop >= 0) {
+        addActiveClass(projectsLink);
+    } 
+    if (windowTop + 500 >= skillsContainer.offsetTop) {
+        addActiveClass(skillsLink);
+    }
+
+    if (windowTop + 300 >= experienceContainer.offsetTop) {
+        addActiveClass(experienceLink);
+    }
+
+    if (windowTop + windowHeight + 300 >= docHeight) {
+        addActiveClass(aboutLink);
+    }
+
+};
+
+const addActiveClass = (activeClass) => {
+    activeClass.classList.add('active');
+    if (projectsLink != activeClass) {
+        projectsLink.classList.remove('active');
+    }
+    if (skillsLink != activeClass) {
+        skillsLink.classList.remove('active');
+    }
+    if (experienceLink != activeClass) {
+        experienceLink.classList.remove('active');
+    }
+    if (aboutLink != activeClass) {
+        aboutLink.classList.remove('active');
+    }
+};
+
+window.addEventListener('scroll', updateNavLinks);
+
+projectsLink.addEventListener('click', updateNavLinks);
+skillsLink.addEventListener('click', updateNavLinks);
+experienceLink.addEventListener('click', updateNavLinks);
+aboutLink.addEventListener('click', updateNavLinks);
+
+window.addEventListener('load', updateNavLinks);
+/*    document.execCommand('copy'); */
